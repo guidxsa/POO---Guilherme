8323f3d932b8129c34b69b2dcab5a26f1839210e
guilherme <guirdesa@gmail.com>
2022-03-28 11:54:54 -0300

    alteração na 19

--- lista 1/Respostas Lista 1 - Guilherme de Sá/l1q19.py
+++ lista 1/Respostas Lista 1 - Guilherme de Sá/l1q19.py
@@ -1,25 +1,24 @@
 n = int(input('Digite o número que deseja saber a raiz quadrada: '))
+a = int(input('Digite uma aproximção a0 da raiz quadrada desse número: '))
 
-a = n ** 0.5
+if n < 0:
+    print('Valor inválido de n')
 
-for contador in range(1, n + 1):
-    if contador < a < contador + 1:
-        a = contador + 0.5
-        break
-    elif a == contador:
-        break
+elif a <= 0:
+    print('Valor inválido de a0')
 
-x = a ** 2 - n
-
-if x < 0:
-    x = -x
-
-
-while x > 0.0001:
-    a = (a + (n / a)) / 2
+else:
     x = a ** 2 - n
-    
+
     if x < 0:
         x = -x
 
-print('A raiz quadrada de {} é {}'.format(n, a))
+
+    while x > 0.0001:
+        a = (a + (n / a)) / 2
+        x = a ** 2 - n
+    
+        if x < 0:
+            x = -x
+
+    print('A raiz quadrada de {} é {}'.format(n, a))
