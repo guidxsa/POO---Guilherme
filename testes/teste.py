b65227332042670800991581fb3eb9e439831aee
Guilherme <guirdesa@gmail.com>
2022-05-06 18:38:49 -0300

    atualizando

--- testes/teste.py
+++ testes/teste.py
@@ -1,9 +1,45 @@
-x = 1
-soma = 0
+data_in = input('Digite data: ')
 
-while x <= 1000:
-    if x % 2 == 1:
-        soma += x
-    x += 1
+mes = None
 
-print('A soma é = ', soma)
+data_sep = data_in.split('/')
+
+if data_sep[1] == '01':
+    mes == 'janeiro'
+
+elif data_sep[1] == '02':
+    mes == 'fev'
+
+elif data_sep[1] == '03':
+    mes == 'mar'
+
+elif data_sep[1] == '04':
+    mes == 'abr'
+
+elif data_sep[1] == '05':
+    mes == 'mai'
+
+elif data_sep[1] == '06':
+    mes == 'jun'
+
+elif data_sep[1] == '07':
+    data_sep[1] == 'jul'
+
+elif data_sep[1] == '08':
+    data_sep[1] == 'ago'
+
+elif data_sep[1] == '09':
+    data_sep[1] == 'set'
+
+elif data_sep[1] == '10':
+    data_sep[1] == 'out'
+
+elif data_sep[1] == '11':
+    data_sep[1] == 'nov'
+
+elif data_sep[1] == '12':
+    data_sep[1] == 'dez'
+
+msg = f'Você nasceu em {data_sep[0]} de {mes} de {data_sep[2]}'
+
+print(msg)
