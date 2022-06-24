1eace5d401856511963988e1ee54b146307df977
guidxsa <92904120+guidxsa@users.noreply.github.com>
2022-06-24 19:47:10 -0300

    Update l4q10.py

--- lista 4/Respostas-Lista4-Guilherme_de_Sa/l4q10.py
+++ lista 4/Respostas-Lista4-Guilherme_de_Sa/l4q10.py
@@ -1,10 +1,17 @@
 class Ingresso:
     def __init__(self, valor):
         self.valor = valor
-        pass
 
     def imprimir_valor(self):
-        pass
+        print('O valor do ingresso é: R${:.2f}'.format(self.valor))
 
 class IngressoVIP(Ingresso):
-    
+    def __init__(self, valor, acrescimo):
+        super().__init__(valor)
+        self.acrescimo = acrescimo
+
+    def set_calcula_VIP(self):
+        self.valor_VIP = self.acrescimo + self.valor
+
+    def get_calcula_VIP(self):
+        return self.valor_VIP 
